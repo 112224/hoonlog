e6508dd5be7224bd56e1b0acfff8348102c378ca
112224 <didgns25@naver.com>
2023-01-16 23:14:07 +0900

    Refactor : 코드 위치 수정

--- src/test/java/com/hoonlog/api/controller/PostControllerTest.java
+++ src/test/java/com/hoonlog/api/controller/PostControllerTest.java
@@ -1,9 +1,10 @@
-package com.hoonlog.controller;
+package com.hoonlog.api.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -23,7 +24,10 @@ class PostControllerTest {
     @DisplayName("/posts 요청시 hello world 출력")
     public void get() throws Exception {
         //given
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"글제목\", \"content\":\"글내용\"}")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
                 .andDo(print());
