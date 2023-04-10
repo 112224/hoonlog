42c7b6e9594d353237ec9ae52ac8529637ac76f7
112224 <didgns25@naver.com>
2023-04-11 08:55:06 +0900

    Feat: 값 검증 추가 및 테스트 진행

--- src/main/java/com/hoonlog/api/request/PostDto.java
+++ src/main/java/com/hoonlog/api/request/PostDto.java
@@ -1,12 +1,18 @@
 package com.hoonlog.api.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Setter
+@Getter
 public class PostDto {
 
+    @NotBlank
     public String title;
+
+    @NotBlank
     public String content;
 }
